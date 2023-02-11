749baf21c03ba51f7131a03dc8fe14f8398afe64
ecrire06 <78669550+ecrire06@users.noreply.github.com>
2023-02-11 04:44:56 +0000

    template inheritance 작업 완료

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, archives, blog, demo, page
 from django.urls import path
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
-  path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+  path('', PostListView.as_view(), name='home'),
+  path('post/<int:pk>', PostDetailView.as_view(), name='single'),
+  path('archives/', archives, name='archives'),
+  path('blog/', blog, name='blog'),
+  path('demo/', demo, name='demo'),
+  path('page/', page, name='page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.views import generic
 from .models import Post
 
+from django.shortcuts import render
 
 class PostListView(generic.ListView):
+    model = Post
     context_object_name = 'post_list'
-    template_name = 'index.html'
+    template_name = 'home.html'
     paginate_by = 4
 
     def get_queryset(self):
@@ -13,4 +15,16 @@ class PostListView(generic.ListView):
 class PostDetailView(generic.DetailView):
   model = Post
   template_name = 'single.html'
-  
+
+def archives(request):
+  return render(request, 'archives.html')
+
+def blog(request):
+  return render(request, 'blog.html')
+
+def demo(request):
+  return render(request, 'demo.html')
+
+def page(request):
+  return render(request, 'page.html')
+
